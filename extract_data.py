26d3f10ce3e6547536a2ba5a5594bc7dde28c504
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2024-03-07 10:15:00 +0530

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -4,7 +4,7 @@ import os
 
 def extract_data():
     s3 = boto3.client('s3')
-    bucket_name = 'deeplearning-mlops'
+    bucket_name = 'deeplearning-mlops-demo'
     url = s3.generate_presigned_url(
                     ClientMethod='get_object',
                     Params={'Bucket': bucket_name, 'Key': 'Training.zip'},
